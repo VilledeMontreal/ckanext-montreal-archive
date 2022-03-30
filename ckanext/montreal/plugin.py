5ee2560e8f8b8459687e72832c4574bbd2216edd
Hristijan Vilos <hristijan.vilos@protonmail.com>
2022-03-30 12:54:47 +0200

    Migrate fanstatic to assets
    Signed-off-by: Hristijan Vilos <hristijan.vilos@protonmail.com>

--- ckanext/montreal/plugin.py
+++ ckanext/montreal/plugin.py
@@ -25,7 +25,7 @@ class MontrealPlugin(plugins.SingletonPlugin):
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic', 'montreal')
+        toolkit.add_resource('assets', 'montreal')
 
     # ITemplateHelpers
 
