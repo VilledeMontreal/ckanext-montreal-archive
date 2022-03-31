9495409892cb918edd2698766e88500cda42d557
Hristijan Vilos <hristijan.vilos@protonmail.com>
2022-03-31 19:20:38 +0200

    Migrate and fix emailer.py
    Signed-off-by: Hristijan Vilos <hristijan.vilos@protonmail.com>

--- ckanext/montreal/emailer.py
+++ ckanext/montreal/emailer.py
@@ -1,13 +1,16 @@
 import logging
 import smtplib
-import cgi
 from socket import error as socket_error
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from email.MIMEBase import MIMEBase
-from email import Encoders
+from email.mime.base import MIMEBase
+from email.encoders import encode_base64
 
-from ckan.common import config
+import werkzeug.datastructures as ds
+
+from ckan.common import config, asbool
+from ckan.lib.mailer import MailerException
+
 
 log = logging.getLogger(__name__)
 
@@ -25,16 +28,17 @@ def send_email(content, to, subject, file=None):
        :rtype: string
 
        '''
-    SMTP_SERVER = config.get('smtp.server')
-    SMTP_USER = config.get('smtp.user')
-    SMTP_PASSWORD = config.get('smtp.password')
-    SMTP_FROM = config.get('smtp.mail_from')
+    smtp_server = config.get('smtp.server')
+    smtp_user = config.get('smtp.user')
+    smtp_password = config.get('smtp.password')
+    smtp_from = config.get('smtp.mail_from')
+    smtp_starttls = asbool(config.get('smtp.starttls'))
 
     msg = MIMEMultipart()
 
-    from_ = SMTP_FROM
+    from_ = smtp_from
 
-    if isinstance(to, basestring):
+    if isinstance(to, str):
         to = [to]
 
     msg['Subject'] = subject
@@ -52,10 +56,10 @@ def send_email(content, to, subject, file=None):
 
     msg.attach(MIMEText(content, 'html', _charset='utf-8'))
 
-    if isinstance(file, cgi.FieldStorage):
+    if isinstance(file, ds.FileStorage):
         part = MIMEBase('application', 'octet-stream')
-        part.set_payload(file.file.read())
-        Encoders.encode_base64(part)
+        part.set_payload(file.stream.read())
+        encode_base64(part)
 
         extension = file.filename.split('.')[-1]
 
@@ -65,21 +69,46 @@ def send_email(content, to, subject, file=None):
         msg.attach(part)
 
     try:
-        s = smtplib.SMTP(SMTP_SERVER)
-        if SMTP_USER:
-            s.login(SMTP_USER, SMTP_PASSWORD)
-        s.sendmail(from_, to, msg.as_string())
-        s.quit()
-        response_dict = {
-            'success': True,
-            'message': 'Email message was successfully sent.'
-        }
-        return response_dict
-    except socket_error:
+        smtp_connection = smtplib.SMTP(smtp_server)
+    except socket_error as e:
         log.critical(
-            'Could not connect to email server. Have you configured the SMTP settings?')
+            'Could not connect to email server. \
+            Have you configured the SMTP settings?')
+        log.exception(e)
         error_dict = {
             'success': False,
             'message': 'An error occured while sending the email. Try again.'
         }
         return error_dict
+    try:
+        # Identify ourselves and prompt the server for supported features.
+        smtp_connection.ehlo()
+
+        if smtp_starttls:
+            if smtp_connection.has_extn('STARTTLS'):
+                smtp_connection.starttls()
+                # Re-identify ourselves over TLS connection.
+                smtp_connection.ehlo()
+            else:
+                raise MailerException("SMTP server does not support STARTTLS")
+
+        # If 'smtp.user' is in CKAN config, try to login to SMTP server.
+        if smtp_user:
+            assert smtp_password, ("If smtp.user is configured then "
+                                   "smtp.password must be configured as well.")
+            smtp_connection.login(smtp_user, smtp_password)
+
+        smtp_connection.sendmail(from_, to, msg.as_string())
+        log.info("Sent email to {0}".format(msg['To']))
+
+    except smtplib.SMTPException as e:
+        msg = '%r' % e
+        log.exception(msg)
+        raise MailerException(msg)
+    finally:
+        smtp_connection.quit()
+        response_dict = {
+            'success': True,
+            'message': 'Email message was successfully sent.'
+        }
+        return response_dict
